b2c5af3830149dc6e1b91e12e8851b7edb75b4b1
VinnieStrong <enzoforte990@gmail.com>
2025-04-01 17:31:10 -0500

    Cretae Display and Render functions

--- script.js
+++ script.js
@@ -56,6 +56,10 @@ const gameLogic = (function() {
         }
     };
 
+    //const play = function() {
+        
+    //}
+
     const checkWin = function() {
         if (
             (gameBoard.board[0] === gameBoard.board[1] && gameBoard.board[1] === gameBoard.board[2]) || 
@@ -70,9 +74,105 @@ const gameLogic = (function() {
         return false;
     };
 
+    const printCurrentPlayer = function() {
+        const currentDisplay = document.createElement('p');
+        currentDisplay.textContent = `${gameLogic.currentPlayer.name} is your turn, make a move!`;
+        document.getElementById('hanger').appendChild(currentDisplay);
+    }
 
-return { switchPlayer, makeMove, checkWin  }
+    const startGame = function() {
+        startButton.addEventListener('click', () => {
+            printCurrentPlayer();
+
+        })
+    }
+
+
+return { switchPlayer, makeMove, checkWin, currentPlayer  }
 })();
+
+const display = (function() {
+    
+    const hanger = document.getElementsByClassName('hanger');
+    const topLeftCell = document.getElementById('top-left');
+    const topCell = document.getElementById('top');
+    const topRightCell = document.getElementById('top-right');
+    const midLeftCell = document.getElementById('mid-left');
+    const centerCell = document.getElementById('center');
+    const midRightCell = document.getElementById('mid-right');
+    const bottomLeftCell = document.getElementById('bottom-left');
+    const bottomCell = document.getElementById('bottom');
+    const bottomRightCell = document.getElementById('bottom-right');
+
+    const render = function() {
+        bottomLeftCell.textContent = `${gameBoard.board[0]}`;
+        bottomCell.textContent = `${gameBoard.board[1]}`;
+        bottomRightCell.textContent = `${gameBoard.board[2]}`;
+        midLeftCell.textContent = `${gameBoard.board[3]}`;
+        centerCell.textContent = `${gameBoard.board[4]}`;
+        midRightCell.textContent = `${gameBoard.board[5]}`;
+        topLeftCell.textContent = `${gameBoard.board[6]}`;
+        topCell.textContent = `${gameBoard.board[7]}`;
+        topRightCell.textContent = `${gameBoard.board[8]}`;
+    }
+
+    topLeftCell.addEventListener('click', () => {
+        gameBoard.board[6] = gameLogic.currentPlayer.mark;
+        render();
+    })
+    topCell.addEventListener('click', () => {
+        gameBoard.board[7] = gameLogic.currentPlayer.mark;
+        render();
+    })
+    topRightCell.addEventListener('click', () => {
+        gameBoard.board[8] = gameLogic.currentPlayer.mark;
+        render();
+    })
+    midLeftCell.addEventListener('click', () => {
+        gameBoard.board[3] = gameLogic.currentPlayer.mark;
+        render();
+        })
+    centerCell.addEventListener('click', () => {
+        gameBoard.board[4] = gameLogic.currentPlayer.mark;
+        render();    
+        })
+    midRightCell.addEventListener('click', () => {
+        gameBoard.board[5] = gameLogic.currentPlayer.mark;
+        render();   
+        })
+    bottomLeftCell.addEventListener('click', () => {
+        gameBoard.board[0] = gameLogic.currentPlayer.mark;
+        render();    
+        })
+    bottomCell.addEventListener('click', () => {
+        gameBoard.board[1] = gameLogic.currentPlayer.mark;
+        render();
+    })
+    bottomRightCell.addEventListener('click', () => {
+        gameBoard.board[2] = gameLogic.currentPlayer.mark;
+        render();
+    })
+
+    return {hanger, gameBoard, render}
+
+
+})();
+
+const test = function() {
+
+}
+
+const startButton = document.createElement('button');
+const buttonText = document.createTextNode('start the game');
+startButton.appendChild(buttonText);
+document.getElementById('hanger').appendChild(startButton);
+startButton.addEventListener('click', () => {
+    const currentDisplay = document.createElement('p');
+    currentDisplay.textContent = `${gameLogic.currentPlayer.name} is your turn`;
+    document.getElementById('hanger').appendChild(currentDisplay);
+})
+
+
 gameLogic.makeMove();
 
 
